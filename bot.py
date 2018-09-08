90462b93faa240e2317b37eb64bf4af9f679adb2
Artyom Barsov <artem_barsov@mail.ru>
2018-09-09 03:52:09 +0500

    converting to json

--- bot.py
+++ bot.py
@@ -14,7 +14,7 @@ bot = telebot.TeleBot(token)
 @bot.message_handler(func=lambda message: True, content_types=['text'])
 def echo_message(message):
     # json_of_message = json.dumps(message)
-    bot.reply_to(message, message.chat)
+    bot.reply_to(message, message)
 
 if "HEROKU" in list(os.environ.keys()):
     logger = telebot.logger
