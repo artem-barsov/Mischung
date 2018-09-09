c4749605367947041e4ed80854501b306755580b
Artyom Barsov <artem_barsov@mail.ru>
2018-09-10 03:39:15 +0500

    converting to json

--- bot.py
+++ bot.py
@@ -13,7 +13,7 @@ bot = telebot.TeleBot(token)
 
 @bot.message_handler(func=lambda message: True, content_types=['text'])
 def echo_message(message):
-    tmpDict = message.__dict__
+    tmpDict = message.to_dict()
     json_of_message = json.dumps(tmpDict)
     bot.reply_to(message, json_of_message)
 
