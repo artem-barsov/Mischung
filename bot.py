fe5600cc902fd3c817e9012b218cef1a7db49132
Artyom Barsov <artem_barsov@mail.ru>
2018-09-11 23:51:50 +0500

    converting to json, testing url

--- bot.py
+++ bot.py
@@ -3,9 +3,6 @@ import telebot
 import logging
 from flask import Flask, request
 
-# try:
-#     import ujson as json
-# except ImportError:
 import json
 
 token = "688343184:AAGnRwbHccoACNsrWr3N75_wnSesvp4t5dA"
@@ -18,9 +15,7 @@ class MyEncoder(JSONEncoder):
 
 @bot.message_handler(func=lambda message: True, content_types=['text'])
 def echo_message(message):
-    # tmpDict = message.__dict__
-    # json_of_message = json.dumps(tmpDict)
-    bot.reply_to(message, MyEncoder().encode(message))
+    bot.reply_to(message, JSONEncoder().encode(message))
 
 if "HEROKU" in list(os.environ.keys()):
     logger = telebot.logger
@@ -35,7 +30,7 @@ if "HEROKU" in list(os.environ.keys()):
     def webhook():
         bot.remove_webhook()
         bot.set_webhook(url="https://tgb-ot.herokuapp.com/bot")
-        return "?", 200
+        return "<h1>?</h1><h2>?</h><h3>?</h><h4>?</h><h5>?</h><h6>?</h>", 200
     server.run(host="0.0.0.0", port=os.environ.get('PORT', 80))
 else:
     bot.remove_webhook()
