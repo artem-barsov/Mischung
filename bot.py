f27f5cb60c2bc08e10d3f0ea82a5eb09dc643fa6
Artyom Barsov <42358981+Artyom-Barsov@users.noreply.github.com>
2019-03-14 20:08:57 +0500

    Back to json

--- bot.py
+++ bot.py
@@ -2,13 +2,14 @@ import os
 import telebot
 import logging
 from flask import Flask, request
+
+# try:
+#     import ujson as json
+# except ImportError:
 import json
-import psycopg2
-import sys
 
 token = "688343184:AAGnRwbHccoACNsrWr3N75_wnSesvp4t5dA"
 bot = telebot.TeleBot(token)
-con = None
 
 from json import JSONEncoder
 class MyEncoder(JSONEncoder):
@@ -17,24 +18,9 @@ class MyEncoder(JSONEncoder):
 
 @bot.message_handler(func=lambda message: True, content_types=['text'])
 def echo_message(message):
-    global con
-    json_of_message = MyEncoder().encode(message)
-    try:
-        con = psycopg2.connect("dbname='Updates'")
-        cur = con.cursor()
-        cur.execute("INSERT INTO Updates(messages) VALUES (%s)", json_of_message)
-        con.commit()
-
-    except psycopg2.DatabaseError as e:
-        if con:
-            con.rollback()
-        print('Error %s' % e)
-        sys.exit(1)
-
-    finally:
-        if con:
-            con.close()
-    bot.reply_to(message, json_of_message)
+    # tmpDict = message.__dict__
+    # json_of_message = json.dumps(tmpDict)
+    bot.reply_to(message, MyEncoder().encode(message))
 
 if "HEROKU" in list(os.environ.keys()):
     logger = telebot.logger
@@ -49,7 +35,7 @@ if "HEROKU" in list(os.environ.keys()):
     def webhook():
         bot.remove_webhook()
         bot.set_webhook(url="https://tgb-ot.herokuapp.com/bot")
-        return "<h1>?</h1><h2>?</h><h3>?</h><h4>?</h><h5>?</h><h6>?</h>", 200
+        return "?", 200
     server.run(host="0.0.0.0", port=os.environ.get('PORT', 80))
 else:
     bot.remove_webhook()
