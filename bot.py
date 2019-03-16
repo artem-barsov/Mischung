ff2cb0e3e59c61ae3f1d3e32df2ea6e0fb776425
Artyom Barsov <42358981+Artyom-Barsov@users.noreply.github.com>
2019-03-16 21:20:21 +0500

    Hide json 3

--- bot.py
+++ bot.py
@@ -16,10 +16,10 @@ class MyEncoder(JSONEncoder):
 showJSON = 'show json : off'
 @bot.message_handler(func=lambda message: True, content_types=['text', 'photo', 'audio', 'video', 'document', 'location', 'contact', 'sticker', 'game', 'video_note', 'voice', 'venue', 'new_chat_member', 'new_chat_members', 'left_chat_member', 'new_chat_title', 'new_chat_photo', 'delete_chat_photo', 'group_chat_created', 'supergroup_chat_created', 'channel_chat_created', 'migrate_to_chat_id', 'migrate_from_chat_id', 'pinned_message', 'invoice', 'successful_payment', 'connected_website'])
 def echo_message(message):
-    markup = types.ReplyKeyboardMarkup()
-    markup.row(quietMode)
-    bot.send_message(message.chat.id, '', reply_markup=markup)
     global showJSON
+    markup = types.ReplyKeyboardMarkup()
+    markup.row(showJSON)
+    bot.send_message(message.chat.id, 'a', reply_markup=markup)
     if message.text == showJSON:
         if message.text == 'show json : off':
             showJSON = 'show json : on'
